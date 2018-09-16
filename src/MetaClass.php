6c88d44b07fa623f7fc836eee343e4ccfd423060
Sergey Boltonosov <minstel@yandex.ru>
2018-09-17 00:07:03 +0700

    Remove empty MetaClass methods

--- src/MetaClass.php
+++ src/MetaClass.php
@@ -64,46 +64,4 @@ class MetaClass extends AbstractMeta
     {
         return $this->properties ?? [];
     }
-
-    /**
-     * Get meta for given class method
-     *
-     * @param string $name
-     * @return mixed
-     */
-    public function getMethod(string $name)
-    {
-
-    }
-
-    /**
-     * Get meta for all class methods
-     *
-     * @return array|null
-     */
-    public function getMethods(): ?array
-    {
-
-    }
-
-    /**
-     * Get meta for class constant
-     *
-     * @param string $name
-     * @return mixed
-     */
-    public function getConstant(string $name)
-    {
-
-    }
-
-    /**
-     * Get meta for all class constants
-     *
-     * @return array|null
-     */
-    public function getConstants(): ?array
-    {
-
-    }
 }
